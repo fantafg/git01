fcf14a998207e41602afe0fb44e1e2ad52815246
Fanta <fantafg@yahoo.com.br>
2016-10-11 12:52:12 -0300

    quatro prints

--- RepGit/src/edu/print/Main.java
+++ RepGit/src/edu/print/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("funcionando");
 		System.out.println("funcionou?");
-		
+		System.out.println("funcionou?");
+		System.out.println("funcionou?");
 	}
 }
